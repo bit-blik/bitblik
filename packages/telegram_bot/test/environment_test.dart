e67bb8fcdbf1e630c3d360680360389cf89f5bc4
anon <anon@anon.com>
2026-09-15 17:52:54 +0200

    fixes

--- packages/telegram_bot/test/environment_test.dart
+++ packages/telegram_bot/test/environment_test.dart
@@ -9,7 +9,12 @@ void main() {
     addTearDown(() => directory.delete(recursive: true));
     final file = File('${directory.path}/.env');
     await file.writeAsString(
-      'PAYMENT_SYSTEM=blik\nFRONTEND_DOMAIN=test.bitblik.app\n',
+      'PAYMENT_SYSTEM=blik\n'
+      'FRONTEND_DOMAIN=test.bitblik.app\n'
+      'EXCLUDED_COORDINATOR_PUBKEYS=npub1excluded\n'
+      'SUBSCRIPTION_ROTATION_SECONDS=1200\n'
+      'OFFER_STATE_RETENTION_SECONDS=3600\n'
+      'MAX_TRACKED_OFFERS=250\n',
     );
 
     final environment = loadTelegramBotEnvironment(
@@ -19,6 +24,10 @@ void main() {
 
     expect(environment['PAYMENT_SYSTEM'], 'twint');
     expect(environment['FRONTEND_DOMAIN'], 'test.bitblik.app');
+    expect(environment['EXCLUDED_COORDINATOR_PUBKEYS'], 'npub1excluded');
+    expect(environment['SUBSCRIPTION_ROTATION_SECONDS'], '1200');
+    expect(environment['OFFER_STATE_RETENTION_SECONDS'], '3600');
+    expect(environment['MAX_TRACKED_OFFERS'], '250');
   });
 
   test('missing .env is optional', () {
