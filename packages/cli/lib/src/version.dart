f25542f32f19335464be8cb082b9b0a89976ac60
anon <anon@anon.com>
2026-09-25 09:35:29 +0200

    v0.11.1

--- packages/cli/lib/src/version.dart
+++ packages/cli/lib/src/version.dart
@@ -1,3 +1,3 @@
 /// CLI release version. Keep in sync with packages/app/pubspec.yaml, excluding
 /// Flutter's app-only build number suffix (for example, `+2040`).
-const cliVersion = '0.11.0';
+const cliVersion = '0.11.1';
